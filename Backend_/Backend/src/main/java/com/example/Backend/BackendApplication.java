70e3c5ee1e51db1b942de2cdbb2d4752edf22465
Jeremy Ascher <jaasch54@gmail.com>
2024-12-22 15:43:38 -0500

    Adjusted main application file.

--- Backend_/Backend/src/main/java/com/example/Backend/BackendApplication.java
+++ Backend_/Backend/src/main/java/com/example/Backend/BackendApplication.java
@@ -1,13 +1,19 @@
 package com.example.Backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
